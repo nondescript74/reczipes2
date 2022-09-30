e0172716f3510be82bddf1ebe07507276a7bad1f
Zahirudeen Premji <headydiscy@gmail.com>
2022-09-30 16:41:18 -0600

    for remote push

---
+++ Reczipes/Helpers/ImagePicker.swift
@@ -0,0 +1,61 @@
+//
+//  ImagePicker.swift
+//  Reczipes
+//
+//  Created by Zahirudeen Premji on 4/20/20.
+//  Copyright © 2020 Zahirudeen Premji. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    
+    typealias UIViewControllerType = UIImagePickerController
+    typealias Coordinator = ImagePickerCoordinator
+    
+    @Binding var image: UIImage?
+    @Binding var isShown: Bool
+    var sourceType: UIImagePickerController.SourceType
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePicker>) {
+    }
+    
+    func makeCoordinator() -> ImagePicker.Coordinator {
+        return ImagePickerCoordinator(image: $image, isShown: $isShown)
+    }
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
+        
+        let picker = UIImagePickerController()
+        picker.sourceType = sourceType
+        picker.delegate = context.coordinator
+        return picker
+        
+    }
+    
+}
+
+class ImagePickerCoordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    @Binding var image: UIImage?
+    @Binding var isShown: Bool
+    
+    init(image: Binding<UIImage?>, isShown: Binding<Bool>) {
+        _image = image
+        _isShown = isShown
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let uiImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            image = uiImage
+            isShown = false
+        }
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        isShown = false
+    }
+}
+
---
+++ Reczipes/Views/ImagePickerView.swift
@@ -0,0 +1,47 @@
+//
+//  ImagePickerView.swift
+//  Reczipes
+//
+//  Created by Zahirudeen Premji on 12/2/20.
+//  Copyright © 2020 Zahirudeen Premji. All rights reserved.
+//
+
+import UIKit
+import SwiftUI
+
+struct ImagePickerView: UIViewControllerRepresentable {
+
+    @Binding var selectedImage: UIImage?
+    @Environment(\.presentationMode) var isPresented
+    var sourceType: UIImagePickerController.SourceType
+        
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = self.sourceType
+        return imagePicker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(picker: self)
+    }
+
+}
+
+class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    var picker: ImagePickerView
+    
+    init(picker: ImagePickerView) {
+        self.picker = picker
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let selectedImage = info[.originalImage] as? UIImage else { return }
+        self.picker.selectedImage = selectedImage
+        self.picker.isPresented.wrappedValue.dismiss()
+    }
+    
+}

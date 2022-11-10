814eb13bfefb728d151e6003c4107e5861843bbc
Zahirudeen Premji <headydiscy@gmail.com>
2022-11-10 14:48:16 -0700

    with fix to units digit display in subviews

--- Reczipes/Views/ExtendedIngredView.swift
+++ Reczipes/Views/ExtendedIngredView.swift
@@ -39,7 +39,7 @@ struct ExtendedIngredView: View {
             ImageAndNameView(extendedIngredient: myExtIngredient)
             MeasuresView(measures: myExtIngredient.measures ?? Measures.measuresExample)
             Text(getMetaInfo(meta: myExtIngredient.metaInformation))
-        }
+        }.padding()
     }
 }
 
@@ -69,10 +69,9 @@ struct ImageAndNameView: View {
     var body: some View {
         HStack(alignment: .center) {
             ImageWithSectionItemView(extendedIngredient: myExtIngredient)
-                Spacer()
             Text(myExtIngredient.name ?? msgs.noIngredName.rawValue)
                 .font(.headline)
-                .padding()
+                .padding(.leading)
         }
     }
 }
@@ -93,53 +92,58 @@ struct MeasuresView: View {
     @EnvironmentObject var ratio: RecipeRatio
     // MARK: - Properties
     fileprivate var myMeasures: Measures!
+    fileprivate var formatter: NumberFormatter = NumberFormatter()
     // MARK: - State
     // MARK: - Methods
     fileprivate func makeRatiodUS() -> Text {
-        let test = myMeasures.us?.amount?.description ?? "0"
+        formatter.maximumSignificantDigits = 2
+        formatter.generatesDecimalNumbers = true
+        let test = myMeasures.us?.amount ?? 0.0
         let testUnits = myMeasures.us?.unitLong ?? "pounds?"
         var testResult: Double = 0
         var testResultUnits: String = testUnits
         switch test {
-        case 0.description:
+        case 0.0:
             break
         default:
             testResult = (myMeasures.us?.amount)! * ratio.ratio
         }
+        let testResultStr = formatter.string(from: testResult as NSNumber) ?? "0.00"
         switch testResultUnits {
         case "pounds?":
             break
         default:
             testResultUnits = (myMeasures.us?.unitLong)!
         }
-        let value = Text(testResult.description) + Text(" ") + Text(testResultUnits)
+        let value = Text(testResultStr) + Text(" ") + Text(testResultUnits)
         return value
     }
     fileprivate func makeRatiodMetric() -> Text {
-        let test = myMeasures.metric?.amount?.description ?? "0"
+        let test = myMeasures.metric?.amount ?? 0.0
         let testUnits = myMeasures.metric?.unitLong ?? "kilos?"
         var testResult: Double = 0
         var testResultUnits: String = testUnits
         switch test {
-        case 0.description:
+        case 0.0:
             break
         default:
             testResult = (myMeasures.metric?.amount)! * ratio.ratio
         }
+        let testResultStr = formatter.string(from: testResult as NSNumber) ?? "0.00"
         switch testResultUnits {
         case "kilos?":
             break
         default:
             testResultUnits = (myMeasures.metric?.unitLong)!
         }
-        let value = Text(testResult.description) + Text(" ") + Text(testResultUnits)
+        let value = Text(testResultStr) + Text(" ") + Text(testResultUnits)
         return value
     }
     // MARK: - View Process
     var body: some View {
         HStack {
             makeRatiodUS() + Text(" or ") + makeRatiodMetric()
-        }.edgesIgnoringSafeArea(.all)
+        }
     }
 }
 
